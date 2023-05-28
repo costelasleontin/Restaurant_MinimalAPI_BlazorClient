4c4afdd1d9685d214af085eddf86cfa4f485b5db
unknown <costelasleontin@yahoo.com>
2023-05-28 20:17:16 +0100

    Second Commit Updated Readme

---
+++ README.md
@@ -0,0 +1,25 @@
+This solution is composed of 2 projects:
+
+- RestaurantsMinimalAPI (a minimal api project type in .net 6)
+- RestaurantsClient ( a client side application project made with blazor webassembly in .net 6)
+
+The RestaurantsMinimalAPI has the following endpoints:
+
+ ![Image](endpoints.png "Minimal API Endpoints")
+
+The RestaurantsMinimalAPI has the following classes:
+
+ ![Image](api_classes.png "Minimal API Classes")
+
+ 1 AddDbContext - provides configuration for the inmemory database used for the example.
+ 2 DataSeeder - provides initial database seeding through the static PrepPopulation method
+ 3 Restaurant, MenuItem and Order - provide the models which are used by the EntityFrameworkCore to work with the database
+
+ The RestaurantsClient has the following app pages:
+ - Index (http://localhost/)- used to show the initial page and show all the restaurants (the restaurant that are outside of active schedule have the link button disabled); the buttons display the pages for individual restaurants
+ - RestaurantMenu (http://localhost/restaurant/{Id:int}) - this is the page for the selected restaurant; this displays menu items and permits adding them to the cart with the "Add to Cart" button; the cart gets updated with the total numbers of picked menu items; the "Checkout" button goes to the Cart page (for Order Completion)
+ - Cart (http://localhost/cart/{Id:int}) - displays the items in the cart for the selected restaurant; items can be remove with the "Remove" button; in the lower part of the page is the order details section which contains a form for user data input like name, address, delivery distance and other mentions; in the order details form there is a "Check Delivery Price" which displays the calculated price for the transport and the total price; the "Submit Order" button submit the completed order and redirects the user to the Index page
+ - AboutUs and ContactUs - provide placeholder extra information about the company
+ - OrderList (http://localhost/orders) - provides a page to visualise all the orders made indifferent of restaurant and all their saved details 
+
+The projects are configured to run on http for development simplicity. The client app doesn't implement form validation, complex exception handling, tests and other production goodies for brevity sake.
--- RestaurantsMinimalAPI/Data/DataSeeder.cs
+++ RestaurantsMinimalAPI/Data/DataSeeder.cs
@@ -34,8 +34,8 @@ public class DataSeeder
                         Name = "PizzaHat",
                         Schedule = "08:00-16:00",
                         MinimumOrderPrice = 10M,
-                        StdDeliveryPrice = 2M,
-                        ExtraDeliveryFeePerKm = 3M,
+                        StdDeliveryPrice = 5M,
+                        ExtraDeliveryFeePerKm = 1.2M,
                         MaximumDistanceStdDelivery = 100,
                     },
                     new Restaurant()
@@ -44,8 +44,8 @@ public class DataSeeder
                         Name = "PizzaRomero",
                         Schedule = "09:00-19:00",
                         MinimumOrderPrice = 10M,
-                        StdDeliveryPrice = 2M,
-                        ExtraDeliveryFeePerKm = 3M,
+                        StdDeliveryPrice = 6.5M,
+                        ExtraDeliveryFeePerKm = 1.4M,
                         MaximumDistanceStdDelivery = 100,
                     },
                     new Restaurant()
@@ -54,8 +54,18 @@ public class DataSeeder
                         Name = "Pizza24",
                         Schedule = "00:00-00:00",
                         MinimumOrderPrice = 10M,
-                        StdDeliveryPrice = 2M,
-                        ExtraDeliveryFeePerKm = 3M,
+                        StdDeliveryPrice = 10M,
+                        ExtraDeliveryFeePerKm = 2M,
+                        MaximumDistanceStdDelivery = 100,
+                    },
+                    new Restaurant()
+                    {
+                        Id = 4,
+                        Name = "AllAroungPizza",
+                        Schedule = "10:00-21:00",
+                        MinimumOrderPrice = 10M,
+                        StdDeliveryPrice = 6M,
+                        ExtraDeliveryFeePerKm = 1.6M,
                         MaximumDistanceStdDelivery = 100,
                     }
                 );
@@ -104,18 +114,34 @@ public class DataSeeder
                         Description = "Vivamus a varius nisl. Ut a bibendum risus, in porta urna. Vivamus consequat tempus augue, vel pretium nunc lobortis in. Pellentesque aliquet, risus ut sollicitudin faucibus, nisl dui pharetra ante, id imperdiet diam diam ut dui. Etiam efficitur condimentum ante, id semper dui pretium in. Vestibulum nec risus in urna tempor elementum. Cras tristique dui nec aliquam faucibus. Nullam blandit mauris mauris, sed tincidunt eros porttitor vel. Donec hendrerit velit ligula, ac dapibus ligula blandit quis. Sed eu ante at est tristique accumsan at id ligula. Phasellus et leo sed eros cursus pellentesque ac vel leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam vel sem rhoncus, varius lorem eu, pharetra massa. Nulla tincidunt diam a nisi consequat, vel tincidunt est blandit. Quisque laoreet sem nibh, id consectetur erat vulputate ac.",
                         Price = 4,
                         RestaurantId = 1
+                    },
+                    new MenuItem()
+                    {
+                        Id = 5,
+                        Name = "Pizza Prosciuto e Fungi",
+                        Description = "Vivamus a varius nisl. Ut a bibendum risus, in porta urna. Vivamus consequat tempus augue, vel pretium nunc lobortis in. Pellentesque aliquet, risus ut sollicitudin faucibus, nisl dui pharetra ante, id imperdiet diam diam ut dui. Etiam efficitur condimentum ante, id semper dui pretium in. Vestibulum nec risus in urna tempor elementum. Cras tristique dui nec aliquam faucibus. Nullam blandit mauris mauris, sed tincidunt eros porttitor vel. Donec hendrerit velit ligula, ac dapibus ligula blandit quis. Sed eu ante at est tristique accumsan at id ligula. Phasellus et leo sed eros cursus pellentesque ac vel leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam vel sem rhoncus, varius lorem eu, pharetra massa. Nulla tincidunt diam a nisi consequat, vel tincidunt est blandit. Quisque laoreet sem nibh, id consectetur erat vulputate ac.",
+                        Price = 4,
+                        RestaurantId = 4
+                    },
+                    new MenuItem()
+                    {
+                        Id = 6,
+                        Name = "Pizza QuatroFromagi",
+                        Description = "Vivamus a varius nisl. Ut a bibendum risus, in porta urna. Vivamus consequat tempus augue, vel pretium nunc lobortis in. Pellentesque aliquet, risus ut sollicitudin faucibus, nisl dui pharetra ante, id imperdiet diam diam ut dui. Etiam efficitur condimentum ante, id semper dui pretium in. Vestibulum nec risus in urna tempor elementum. Cras tristique dui nec aliquam faucibus. Nullam blandit mauris mauris, sed tincidunt eros porttitor vel. Donec hendrerit velit ligula, ac dapibus ligula blandit quis. Sed eu ante at est tristique accumsan at id ligula. Phasellus et leo sed eros cursus pellentesque ac vel leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam vel sem rhoncus, varius lorem eu, pharetra massa. Nulla tincidunt diam a nisi consequat, vel tincidunt est blandit. Quisque laoreet sem nibh, id consectetur erat vulputate ac.",
+                        Price = 4,
+                        RestaurantId = 4
                     }
                 );
 
-            context.SaveChanges();
-        }
-        else
-        {
-            Console.WriteLine("--> We already have menu item data");
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have menu item data");
+            }
+
         }
 
     }
-
-}
 
 }
